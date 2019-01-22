a7eacd5e80b594779579563d631ddd45faa03b51
Bumilka <makowskafashion@gmail.com>
2019-01-22 22:07:26 +0100

    changed files

--- The Game/GameEngine.js
+++ The Game/GameEngine.js
@@ -1,3 +1,4 @@
+/*
 // class - pogrupowanie zmiennych i funkcji; programowanie obiektowe;
 class GameEngine{
     constructor(){
@@ -23,4 +24,43 @@ class GameEngine{
 
 }
 
+*/
 
+
+// definicja obiektu typu Player albo prototyp
+class Player
+{
+    constructor(spriteId)
+    {
+        this.health = 100;
+        this.sprite = document.getElementById(spriteId);
+        this.sprite.style.top = 100  + 'px';
+        this.sprite.style.left = 100 + 'px'; 
+    };
+
+    Move(){
+        console.log("dupa");
+     }
+}
+class GameEngine
+{
+    constructor()
+    {
+        console.log("STWORZYLEM GAME ENGINE");
+        this.player = new Player('hero');
+        this.enemies = new Array();  
+    }
+    ListenForKeysPressed(){
+        document.onkeydown = function(evt) {
+            console.log(evt);
+        }
+    };
+    loop() {
+        this.ListenForKeysPressed();
+    };
+}
+
+
+// deklaracja biektu typu GameEngine
+var game = new GameEngine();
+setInterval(game.loop, 1000);
