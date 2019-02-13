3ecfb5d358d810ddb5375bc9250edcee4723cf54
Marcin Osak <marcin@avangardo.pl>
2019-02-13 16:24:54 +0100

    Adding death card

--- The Game/manual.js
+++ The Game/manual.js
@@ -1,9 +1,11 @@
 let playBtn = document.querySelector("#game_starter"); 
 let helpBtn = document.querySelector("#help_button"); 
+let restartBtn = document.querySelector("#game_restarter"); 
 
-function viewManual (action){
-    document.querySelector(".container__game_rules").style.display = action;
+function cardDisplayer (card, action){
+    document.querySelector(card).style.display = action;
 }
 
-playBtn.addEventListener("click", () => viewManual("none")); 
-helpBtn.addEventListener("click", () => viewManual("block")); 
+playBtn.addEventListener("click", () => cardDisplayer(".container__game_rules", "none"));  
+helpBtn.addEventListener("click", () => cardDisplayer(".container__game_rules", "block")); 
+restartBtn.addEventListener("click", () => cardDisplayer(".container__game_over", "none")); 
