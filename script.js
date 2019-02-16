abd816be483fbc3eec1018e349d2324adca4e0ab
3xoses <sebastian.staniak@interia.pl>
2019-02-16 21:42:59 +0100

    add new calculator

--- script.js
+++ script.js
@@ -55,14 +55,23 @@ for(var i = 0; i < keys.length; i++) {
 	} 
 }
 
+function sumValues() {
+	let num1, num2, res;
+	num1 = Number(document.calculator.txtnum1.value);
+	num2 = Number(document.calculator.txtnum2.value);
+	res = num1 + num2 + ' zł';
+	document.calculator.txtres.value = res;
+}
+
 let btnShowCalculator = document.querySelector('.check-paid');
 let btnHideCalculator = document.querySelector('.check-paid-hide');
-let hiddenCalculator = document.querySelector('#calculator');
+let hiddenCalculator = document.querySelector('form');
 
 btnShowCalculator.addEventListener('click', function() {
    hiddenCalculator.style.display = "block";
 });
 
 btnHideCalculator.addEventListener('click', function() {
-    hiddenCalculator.style.display = "none";
+	hiddenCalculator.style.display = "none";
+	hiddenCalculator.reset();
 });
