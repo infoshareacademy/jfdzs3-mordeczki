d885e01e60987bb1d4fecba41ee39cee3e09592e
filadam <filip.adamczyk90@gmail.com>
2019-02-17 12:39:43 +0100

    scrolling added, bugs fixed

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-$(function(){
-    $('.selectpicker').selectpicker();
-});
+// $(function(){
+//     $('.selectpicker').selectpicker();
+// });
+// nie działa^^^
 
 //cookies//
 CookieAlert.init();
 
-//animation//
+//calculator
+function sumValues() {
+	let num1, num2, res;
+	num1 = Number(document.calculator.txtnum1.value);
+	num2 = Number(document.calculator.txtnum2.value);
+	res = num1 + num2 + ' zł';
+	document.calculator.txtres.value = res;
+}
+
+let btnShowCalculator = document.querySelector('.check-paid');
+let btnHideCalculator = document.querySelector('.check-paid-hide');
+let hiddenCalculator = document.querySelector('.calculator');
+
+btnShowCalculator.addEventListener('click', function () {
+	hiddenCalculator.style.display = "block";
+});
+
+btnHideCalculator.addEventListener('click', function () {
+	hiddenCalculator.style.display = "none";
+	hiddenCalculator.reset();
+});
+
+
+// scrollowanie
+
+$(document).ready(function () {
+	$("a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+});
